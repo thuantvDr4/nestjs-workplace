be448c53724d39bf6a36411b8fce9867dcce0d0f
Tran Vinh Thuan <thuan.tran@bstarsolutions.com>
2025-08-19 13:23:12 +0700

    save

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,68 @@
-import { Controller } from '@nestjs/common';
+/**
+ * Noi quan ly endpoint
+ *
+ */
+
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  public getUsers() {
+    return 'Get all users';
+  }
+
+  @Get('/:id')
+  public getUserById(
+    @Param('id', ParseIntPipe) id: number,
+    @Query('limit', ParseIntPipe) limit?: number,
+    @Query('ofset', ParseIntPipe) ofset?: number,
+    @Query('role') role?: string,
+  ) {
+    console.log('----id:', typeof id);
+    console.log('----limit:', limit);
+    console.log('----ofset:', ofset);
+    console.log('----role:', role);
+    return 'You sent a GET request to get user detail by id: ' + id;
+  }
+
+  //@Body => lây đu thong tin can
+  // @Req => lay toan bo: body, header,cookies, ...
+  @Post()
+  public createUser(
+    @Body('email') email: string,
+    @Body('name') name: string,
+    @Body('phone') phone: string,
+  ) {
+    console.log('----email', email);
+    console.log('----name', name);
+    console.log('----phone', phone);
+    return 'You sent a Post request to create new user';
+  }
+
+  @Put()
+  public updateUser() {
+    return 'You sent a PUT request to update user';
+  }
+
+  @Patch()
+  public updateuser_v2() {
+    return 'You sent a PATCH request to update user';
+  }
+
+  @Delete()
+  public deleteUser() {
+    return 'You sent a DELETE request to remove user';
+  }
+}
